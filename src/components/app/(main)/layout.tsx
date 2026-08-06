f7b3d1db18c91436d37d3b59003bef0a69fbe32c
dmsdn0424 <has_26127@hana.hs.kr>
2026-08-06 19:12:20 +0900

    Delete src/components/app directory

--- src/components/app/(main)/layout.tsx
+++
@@ -1,5 +0,0 @@
-import MainLayoutClient from "@/components/layout/MainLayoutClient";
-
-export default function MainLayout({ children }: { children: React.ReactNode }) {
-  return <MainLayoutClient>{children}</MainLayoutClient>;
-}
