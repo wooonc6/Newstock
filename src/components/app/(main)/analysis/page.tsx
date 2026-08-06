658ae0889190e293885554e9f1b62c0940c8fefa
dmsdn0424 <has_26127@hana.hs.kr>
2026-08-06 19:06:26 +0900

    Update page.tsx

--- src/components/app/(main)/analysis/page.tsx
+++ src/components/app/(main)/analysis/page.tsx
@@ -1,23 +1,13 @@
-import AnalysisTabs from "@/components/analysis/AnalysisTabs";
-import InvestmentPerformance from "./InvestmentPerformance";
-import QuizAnalysis from "./QuizAnalysis";
-
-import HistoryPage from "../history/page";
-
-export const dynamic = "force-dynamic";
-
-export default async function AnalysisPage({
-  searchParams,
-}: {
-  searchParams: Promise<{ recordTicker?: string }>;
-}) {
-  const { recordTicker } = await searchParams;
-
+export default function AnalysisPage() {
   return (
-    <AnalysisTabs
-      growth={<InvestmentPerformance />}
-      quiz={<QuizAnalysis />}
-      history={<HistoryPage selectedTicker={recordTicker} />}
-    />
+    <div
+      style={{
+        fontSize: 50,
+        color: "red",
+        fontWeight: 900,
+      }}
+    >
+      TEST123456
+    </div>
   );
 }
